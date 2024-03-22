5792cb55ba4fb4e777e12c1438f5b78024590221
Virtus_Chae <ybd4731@gmail.com>
2024-03-23 01:21:52 +0900

    add : 컬렉션 목차 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-str_a = "Hello"
-str_b = "JamCoding"
-result = str_a + "\n" + str_b
-print(result)
+dictionary_a = {"이름" : "유재석", "나이" : 5}
+dictionary_b = {"apple" : "사과", "banana" : "바나나"}
+print(dictionary_a)
+print(dictionary_b)
+print(dictionary_a + dictionary_b) # 결과를 확인해 보자.
