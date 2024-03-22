336c8afdfcb37717715397572b2f008248beff58
Virtus_Chae <ybd4731@gmail.com>
2024-03-22 23:39:47 +0900

    feat : 2장까지 교재 준비 완료

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-print('''\    /\\
- )  ( ')
-(  /  )
- \(__)|
- ''')
+myAge = 15
+yourAge = 13
+PI = 3.14
+first_Name = "Ryu"
+last_Name = 'Hyun Jin'
+변수 = 5
+
+print(myAge)
+print(yourAge)
+print(PI)
+print(first_Name)
+print(last_Name)
+print(변수)
