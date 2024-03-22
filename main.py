0d9780e39ddd392463cd2cb60e5c3b97ed631280
Virtus_Chae <ybd4731@gmail.com>
2024-03-23 00:22:33 +0900

    add : 숫자형과 문자형 파일 업로드

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-myAge = 15
-yourAge = 13
-PI = 3.14
-first_Name = "Ryu"
-last_Name = 'Hyun Jin'
-변수 = 5
-
-print(myAge)
-print(yourAge)
-print(PI)
-print(first_Name)
-print(last_Name)
-print(변수)
+str_a = "Hello"
+str_b = "JamCoding"
+result = str_a + "\n" + str_b
+print(result)
