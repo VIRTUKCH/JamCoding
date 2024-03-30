a5b484efd7c7c55e37263c3d4cba64356911bcd6
Virtus_Chae <ybd4731@gmail.com>
2024-03-30 22:06:25 +0900

    교육 자료 업데이트

--- main.py
+++
@@ -1,5 +0,0 @@
-dictionary_a = {"이름" : "유재석", "나이" : 5}
-dictionary_b = {"apple" : "사과", "banana" : "바나나"}
-print(dictionary_a)
-print(dictionary_b)
-print(dictionary_a + dictionary_b) # 결과를 확인해 보자.
