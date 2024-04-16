56af2918601adbcec17a63232077b3b5f1b11eca
Virtus_Chae <ybd4731@gmail.com>
2024-04-16 10:54:35 +0900

    Main branch commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Main Branch");
+    }
+}
