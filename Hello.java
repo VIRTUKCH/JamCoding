0426690cbfdcecb2758aa148fc0fc5d645f4f7a3
Virtus_Chae <ybd4731@gmail.com>
2024-04-16 11:11:15 +0900

    help branch서 .java 삭제

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello JAVA");
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Main Branch");
-    }
-}
