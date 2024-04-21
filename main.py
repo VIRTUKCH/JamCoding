24301890986315692852f3cb5bf9f878c5bfd31a
Virtus_Chae <ybd4731@gmail.com>
2024-04-21 16:12:48 +0900

    python : 함수 관련 강의안 추가

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-# 첫째 줄에는 별 2×N-1개, 둘째 줄에는 별 2×N-3개, ..., N번째 줄에는 별 1개를 찍는 문제
+# 문제 5
+# 주어진 숫자가 소수인지 확인하는 함수 정의
+def is_prime(number):
+    if number <= 1:  # 주어진 숫자가 1 이하인 경우
+        return False  # 소수가 아니므로 False 반환
+    for i in range(2, number):  # 2부터 number-1까지의 수에 대해 반복
+        if number % i == 0:  # number가 i로 나누어 떨어지는 경우
+            return False  # 소수가 아니므로 False 반환
+    return True  # 위의 경우에 해당하지 않으면 소수이므로 True 반환
 
-# N = 3
-# *****
-# ***
-# *
+def calculate_average(list):
+    total = 0  # 총합을 저장할 변수를 초기화합니다.
+    for i in list:  # 리스트 l의 각 요소를 반복하며
+        total += i  # 요소를 총합에 더합니다.
+    count = len(list)  # 리스트의 요소 개수를 구합니다.
+
+    return total / count  # 평균을 계산하여 반환합니다.
+
+sum = 0
+list = []
+for i in range(100):
+    if(is_prime(i) == True):
+        list.append(i)
+result = calculate_average(list)
+print(result)
